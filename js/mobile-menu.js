350e8c334871cc101debc954e81c00ea408d66b8
avinashrautai <avinash.raut@outlook.com>
2026-06-03 11:50:51 +0530

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,1 +1,40 @@
 
+const toggle =
+document.querySelector(".mobile-toggle");
+
+const menu =
+document.querySelector(".mobile-menu");
+
+const closeBtn =
+document.querySelector(".mobile-close");
+
+if(toggle && menu){
+
+toggle.addEventListener("click",()=>{
+
+menu.classList.add("active");
+
+});
+
+}
+
+if(closeBtn && menu){
+
+closeBtn.addEventListener("click",()=>{
+
+menu.classList.remove("active");
+
+});
+
+}
+
+document.querySelectorAll(".mobile-menu a")
+.forEach(link => {
+
+link.addEventListener("click",()=>{
+
+menu.classList.remove("active");
+
+});
+
+});
